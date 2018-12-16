4c7f37b138222bf32285ab3af6a08e7bbd4e3f46
VillanuevaCruzJesusAntonio <mrvillantocruz@gmail.com>
2018-12-15 20:40:24 -0600

    Create Cifrador.py

---
+++ cifrado/escritorio/c-digo/Python/Cifrador.py
@@ -0,0 +1,38 @@
+from Texto.TextoPlano import TextoPlano
+from Cifrados.cifradoCesar import CifradoCesar
+from Cifrados.cifradoTInversa import CifradoTInversa
+from Cifrados.cifradoTGrupo import CifradoTGrupo
+
+class Cifrador:
+
+    def __init__(self):
+        self.textoEntrada = TextoPlano("")
+        self.cCesar = CifradoCesar("", 3)
+        self.cTG = CifradoTGrupo("", 3)
+        self.cTI = CifradoTInversa("")
+        self.array = ""
+
+    def datoExternoTXT(self, nuevo ,val ,val2):
+        self.array = nuevo
+        estText = TextoPlano(nuevo)
+        self.textoEntrada.difDimensiones(nuevo)
+
+        self.evenCifradoTInversa(estText,nuevo)
+        self.evenCifradoCesar(nuevo ,val)
+        self.evenCifradoTGrupo(nuevo ,val2)
+
+        self.textoEntrada.refresh(nuevo)
+
+    def evenCifradoTInversa(self ,estText,txt):
+        if self.textoEntrada.backOne(estText):
+            self.cTI.back()
+        elif self.textoEntrada.step(estText):
+            self.cTI.front(self.textoEntrada.textoDiferen)
+        else:
+            self.cTI.cifra(txt)
+
+    def evenCifradoCesar(self ,txt,salto):
+        self.cCesar.cifrar(txt,salto)
+
+    def evenCifradoTGrupo(self ,txt,group):
+        self.cTG.cifrar(txt,group)
